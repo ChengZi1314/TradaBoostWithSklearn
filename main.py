00d53b3f9dfb9ee129387613d540916693b63a9e
Cheng_Zi <zyk_cuc@163.com>
2019-01-22 14:03:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import numpy as np
 from code.models import TradaBoostWithXGB, TradaBoostWithLGB
 from sklearn import preprocessing
 
-sizeList = [1000, 10000, 20000, 50000, 100000, 200000, 500000]
-
 dfTrainTarget = pd.read_csv("../dataTransfer/11405010.csv", index_col=None)
 dfTrainAssist = pd.read_csv("../dataTransfer/11406010.csv", index_col=None)
 dfTest = pd.read_csv("../dataTransfer/11405010test.csv", index_col=None)
